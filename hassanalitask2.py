57b2b7d3c4ba5375f363c2e322ee07216a0939ab
hassanhussain381 <ha263428@gmail.com>
2024-08-25 18:41:33 +0500

    Add files via upload

---
+++ hassanalitask2.py
@@ -0,0 +1,74 @@
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+
+stop_words = set([
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", 
+    "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 
+    'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 
+    'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 
+    'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 
+    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 
+    'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 
+    'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 
+    'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 
+    'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 
+    'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 
+    'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 
+    'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 
+    'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', 
+    "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', 
+    "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 
+    'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', 
+    "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"
+])
+
+
+def preprocess_tweet(tweet):
+   
+    tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+    tweet = re.sub(r'\@\w+|\#', '', tweet)
+    tweet = re.sub(r'[^A-Za-z0-9]+', ' ', tweet)
+    
+   
+    tweet = tweet.lower()
+    
+   
+    tweet = ' '.join([word for word in tweet.split() if word not in stop_words])
+    
+    return tweet
+
+
+tweets = [
+    "I love this product! #happy",
+    "This is the worst experience I've ever had.",
+    "I'm so excited about the new update.",
+    "I hate waiting in line."
+]
+
+
+labels = [1, 0, 1, 0]
+
+
+tweets = [preprocess_tweet(tweet) for tweet in tweets]
+
+
+vectorizer = TfidfVectorizer(max_features=1000)
+X = vectorizer.fit_transform(tweets)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
